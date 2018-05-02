ae86ac1a0844916791626a27d543c10788eb5c5b
yuichi.watanabe <yuichi.watanabe@nulab-inc.com>
2018-05-02 18:50:29 +0900

    #7 implements notification api and test

---
+++ typetalk/v2/notifications.go
@@ -0,0 +1,109 @@
+package v2
+
+import (
+	"context"
+
+	"time"
+
+	. "github.com/nulab/go-typetalk/typetalk/shared"
+)
+
+type NotificationsService service
+
+type ReadNotificationResult struct {
+	Space  *Space  `json:"space"`
+	Access *Access `json:"access"`
+}
+
+type Space struct {
+	Key      string `json:"key"`
+	Name     string `json:"name"`
+	Enabled  bool   `json:"enabled"`
+	ImageURL string `json:"imageUrl"`
+}
+
+type Access struct {
+	Unopened          int `json:"unopened"`
+	UnopenedExcludeDM int `json:"unopenedExcludeDM"`
+}
+
+type NotificationCount struct {
+	Statuses []*struct {
+		MySpace *MySpace `json:"mySpace"`
+		Space   *Space   `json:"space"`
+		Access  *Access  `json:"access"`
+		Like    *struct {
+			Receive *struct {
+				HasUnread  bool `json:"hasUnread"`
+				ReadLikeID int  `json:"readLikeId"`
+			} `json:"receive"`
+		} `json:"like"`
+		DirectMessage *struct {
+			UnreadTopics int `json:"unreadTopics"`
+		} `json:"directMessage"`
+	} `json:"statuses"`
+	DoNotDisturb *DoNotDisturb `json:"doNotDisturb"`
+}
+
+type MySpace struct {
+	Space          *Space   `json:"space"`
+	MyRole         string   `json:"myRole"`
+	IsPaymentAdmin bool     `json:"isPaymentAdmin"`
+	InvitableRoles []string `json:"invitableRoles"`
+	MyPlan         MyPlan   `json:"myPlan"`
+}
+
+type MyPlan struct {
+	Plan                *Plan       `json:"plan"`
+	Enabled             bool        `json:"enabled"`
+	Trial               interface{} `json:"trial"`
+	NumberOfUsers       int         `json:"numberOfUsers"`
+	TotalAttachmentSize int         `json:"totalAttachmentSize"`
+	CreatedAt           *time.Time  `json:"createdAt"`
+	UpdatedAt           *time.Time  `json:"updatedAt"`
+}
+
+type Plan struct {
+	Key                      string `json:"key"`
+	Name                     string `json:"name"`
+	LimitNumberOfUsers       int    `json:"limitNumberOfUsers"`
+	LimitTotalAttachmentSize int    `json:"limitTotalAttachmentSize"`
+}
+
+type DoNotDisturb struct {
+	IsSuppressed bool       `json:"isSuppressed"`
+	Manual       *Manual    `json:"manual"`
+	Scheduled    *Scheduled `json:"scheduled"`
+}
+
+type Manual struct {
+	RemainingTimeInMinutes interface{} `json:"remainingTimeInMinutes"`
+}
+
+type Scheduled struct {
+	Enabled bool   `json:"enabled"`
+	Start   string `json:"start"`
+	End     string `json:"end"`
+}
+
+// Typetalk API docs: https://developer.nulab-inc.com/docs/typetalk/api/2/get-notification-status
+func (s *NotificationsService) GetNotificationCount(ctx context.Context) (*NotificationCount, *Response, error) {
+	u := "notifications/status"
+	var result *NotificationCount
+	if resp, err := s.client.Get(ctx, u, &result); err != nil {
+		return nil, resp, err
+	} else {
+		return result, resp, nil
+	}
+}
+
+// Typetalk API docs: https://developer.nulab-inc.com/ja/docs/typetalk/api/2/open-notification
+func (s *NotificationsService) ReadNotification(ctx context.Context) (*ReadNotificationResult, *Response, error) {
+	u := "notifications"
+	var result *ReadNotificationResult
+	if resp, err := s.client.Put(ctx, u, nil, &result); err != nil {
+		return nil, resp, err
+	} else {
+		return result, resp, nil
+	}
+}
---
+++ typetalk/v2/notifications_test.go
@@ -0,0 +1,53 @@
+package v2
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func Test_NotificationsService_GetNotificationCount_should_get_notification_count(t *testing.T) {
+	setup()
+	defer teardown()
+	b, _ := ioutil.ReadFile(fixturesPath + "get-notification-count.json")
+	mux.HandleFunc("/notifications/status",
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, "GET")
+			fmt.Fprint(w, string(b))
+		})
+
+	result, _, err := client.Notifications.GetNotificationCount(context.Background())
+	if err != nil {
+		t.Errorf("Returned error: %v", err)
+	}
+	var want *NotificationCount
+	json.Unmarshal(b, &want)
+	if !reflect.DeepEqual(result, want) {
+		t.Errorf("Returned result:\n result  %v,\n want %v", result, want)
+	}
+}
+
+func Test_NotificationsService_ReadNotification_should_read_notification(t *testing.T) {
+	setup()
+	defer teardown()
+	b, _ := ioutil.ReadFile(fixturesPath + "read-notification.json")
+	mux.HandleFunc("/notifications",
+		func(w http.ResponseWriter, r *http.Request) {
+			testMethod(t, r, "PUT")
+			fmt.Fprint(w, string(b))
+		})
+
+	result, _, err := client.Notifications.ReadNotification(context.Background())
+	if err != nil {
+		t.Errorf("Returned error: %v", err)
+	}
+	var want *ReadNotificationResult
+	json.Unmarshal(b, &want)
+	if !reflect.DeepEqual(result, want) {
+		t.Errorf("Returned result:\n result  %v,\n want %v", result, want)
+	}
+}
