e78cca05ac9a4e7a4f511079cb27a6d3bdf9ea42
yuichi.watanabe <yuichi.watanabe@nulab-inc.com>
2018-05-08 00:58:08 +0900

    update integration test (#13)

--- typetalk/v2/messages.go
+++ typetalk/v2/messages.go
@@ -2,6 +2,7 @@ package v2
 
 import (
 	"context"
+
 	"time"
 
 	. "github.com/nulab/go-typetalk/typetalk/internal"
@@ -11,28 +12,28 @@ import (
 type MessagesService service
 
 type SearchMessagesResult struct {
-	Count int `json:"count"`
-	Posts []*Post `json:"posts"`
-	IsLimited bool `json:"isLimited"`
+	Count     int     `json:"count"`
+	Posts     []*Post `json:"posts"`
+	IsLimited bool    `json:"isLimited"`
 }
 
 type SearchMessagesOptions struct {
-	TopicIDs []int `json:"topicIds,omitempty"`
-	HasAttachments bool `json:"hasAttachments,omitempty"`
-	AccountIDs []int `json:"accountIds,omitempty"`
-	From *time.Time `json:"from,omitempty"`
-	To *time.Time `json:"to,omitempty"`
+	TopicIDs       []int      `json:"topicIds,omitempty"`
+	HasAttachments bool       `json:"hasAttachments,omitempty"`
+	AccountIDs     []int      `json:"accountIds,omitempty"`
+	From           *time.Time `json:"from,omitempty"`
+	To             *time.Time `json:"to,omitempty"`
 }
 
 type searchMessagesOptions struct {
 	*SearchMessagesOptions
 	SpaceKey string `json:"spaceKey"`
-	Q string `json:"q"`
+	Q        string `json:"q"`
 }
 
 // Typetalk API docs: https://developer.nulab-inc.com/docs/typetalk/api/2/search-messages/
 func (s *MessagesService) SearchMessages(ctx context.Context, spaceKey, q string, opt *SearchMessagesOptions) (*SearchMessagesResult, *Response, error) {
-	u, err := AddQueries("search/posts", &searchMessagesOptions{SearchMessagesOptions: opt, SpaceKey: spaceKey, Q: q })
+	u, err := AddQueries("search/posts", &searchMessagesOptions{SearchMessagesOptions: opt, SpaceKey: spaceKey, Q: q})
 	if err != nil {
 		return nil, nil, err
 	}
