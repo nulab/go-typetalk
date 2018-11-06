c8a7aaa264764634cd34b78ce6cba3cb7f679b39
Issei Horie <issei.horie@nulab-inc.com>
2018-11-07 01:02:27 +0900

    Support /api/v5/notifications/status

---
+++ typetalk/v5/v5.go
@@ -0,0 +1,42 @@
+package v5
+
+import (
+	"net/http"
+	"net/url"
+
+	"github.com/nulab/go-typetalk/typetalk/internal"
+)
+
+const (
+	ApiVersion = "v5"
+)
+
+type service struct {
+	client *internal.ClientCore
+}
+
+type Client struct {
+	client *internal.ClientCore
+
+	Notifications *NotificationsService
+}
+
+func (c *Client) SetTypetalkToken(token string) *Client {
+	c.client.TypetalkToken = token
+	return c
+}
+
+func NewClient(httpClient *http.Client) *Client {
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+	baseURL, _ := url.Parse(internal.DefaultBaseURL + ApiVersion + "/")
+
+	c := &Client{client: &internal.ClientCore{Client: httpClient, BaseURL: baseURL, UserAgent: internal.UserAgent}}
+
+	common := &service{client: c.client}
+
+	c.Notifications = (*NotificationsService)(common)
+
+	return c
+}
