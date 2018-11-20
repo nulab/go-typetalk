99c97e481ce12ed9ee572b6fe66a481acb45a8d6
Issei Horie <issei.horie@nulab-inc.com>
2018-11-21 02:17:02 +0900

    Support /api/v3/notifications

---
+++ typetalk/v3/notifications.go
@@ -0,0 +1,40 @@
+package v3
+
+import (
+	"context"
+
+	. "github.com/nulab/go-typetalk/typetalk/shared"
+)
+
+type NotificationsService service
+
+type ReadNotificationResult struct {
+	Space  *Space  `json:"space"`
+	Access *Access `json:"access"`
+}
+
+type Space struct {
+	Key      string `json:"key"`
+	Name     string `json:"name"`
+	Enabled  bool   `json:"enabled"`
+	ImageURL string `json:"imageUrl"`
+}
+
+type Access struct {
+	Unopened int `json:"unopened"`
+}
+
+type readNotificationOptions struct {
+	SpaceKey string `json:"spaceKey"`
+}
+
+// Typetalk API docs: https://developer.nulab-inc.com/ja/docs/typetalk/api/3/open-notification
+func (s *NotificationsService) ReadNotification(ctx context.Context, spaceKey string) (*ReadNotificationResult, *Response, error) {
+	u := "notifications"
+	var result *ReadNotificationResult
+	if resp, err := s.client.Put(ctx, u, &readNotificationOptions{spaceKey}, &result); err != nil {
+		return nil, resp, err
+	} else {
+		return result, resp, nil
+	}
+}
