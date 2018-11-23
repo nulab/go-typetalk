a5879c207823ee346b37bf0d4cbde9a8951c9d92
Issei Horie <issei.horie@nulab-inc.com>
2018-11-23 20:28:05 +0900

    go fmt

--- tests/integration/likes_test.go
+++ tests/integration/likes_test.go
@@ -20,18 +20,17 @@ func Test_V1_Likes_GetLikesDiscover_should_get_some_DiscoverLikedPost(t *testing
 	test(t, result, resp, err)
 }
 
-
 func Test_V2_Likes_GetLikesGive_should_get_some_GiveLikedPost(t *testing.T) {
 	result, resp, err := clientV2.Likes.GetLikesGive(context.Background(), spaceKey, nil)
 	test(t, result, resp, err)
 }
 
 func Test_V2_Likes_GetLikesReceive_should_get_some_ReceiveLikedPost(t *testing.T) {
-	result, resp, err := clientV2.Likes.GetLikesReceive(context.Background(), spaceKey,nil)
+	result, resp, err := clientV2.Likes.GetLikesReceive(context.Background(), spaceKey, nil)
 	test(t, result, resp, err)
 }
 
 func Test_V2_Likes_GetLikesDiscover_should_get_some_DiscoverLikedPost(t *testing.T) {
-	result, resp, err := clientV2.Likes.GetLikesDiscover(context.Background(), spaceKey,nil)
+	result, resp, err := clientV2.Likes.GetLikesDiscover(context.Background(), spaceKey, nil)
 	test(t, result, resp, err)
 }
