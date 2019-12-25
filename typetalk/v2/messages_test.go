53325da7d5a7fac81ca53ec991565859435421cf
Issei Horie <is2ei.horie@gmail.com>
2019-12-26 01:43:23 +0900

    Improved coverage v2/messages.go

--- typetalk/v2/messages_test.go
+++ typetalk/v2/messages_test.go
@@ -42,6 +42,18 @@ func Test_MessagesService_GetDirectMessages_should_get_some_direct_messages(t *t
 	}
 }
 
+func Test_MessagesService_GetDirectMessages_errorResponse(t *testing.T) {
+	_, _, err := client.Messages.GetDirectMessages(context.Background(), "qwerty", "test", &GetMessagesOptions{
+		10,
+		1,
+		"backword",
+	})
+
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
+
 func Test_MessagesService_PostDirectMessage_should_post_dairect_message(t *testing.T) {
 	setup()
 	defer teardown()
@@ -89,6 +101,21 @@ func Test_MessagesService_PostDirectMessage_should_post_dairect_message(t *testi
 	}
 }
 
+func Test_MessagesService_PostDirectMessage_errorResponse(t *testing.T) {
+	_, _, err := client.Messages.PostDirectMessage(context.Background(), "qwerty", "test", "hello", &PostMessageOptions{
+		ReplyTo:      2,
+		ShowLinkMeta: true,
+		FileKeys:     []string{"key0", "key1", "key2"},
+		TalkIds:      []int{0, 1, 2},
+		FileUrls:     []string{"Url0", "Url1", "Url2"},
+		FileNames:    []string{"Name0", "Name1", "Name2"},
+	})
+
+	if err == nil {
+		t.Errorf("Expected error to be returned")
+	}
+}
+
 func Test_MessagesService_SearchMessages_should_get_some_posts(t *testing.T) {
 	setup()
 	defer teardown()
@@ -117,5 +144,16 @@ func Test_MessagesService_SearchMessages_should_get_some_posts(t *testing.T) {
 	json.Unmarshal(b, want)
 	if !reflect.DeepEqual(result, want) {
 		t.Errorf("Returned result:\n result  %v,\n want %v", result, want)
+	}
+}
+
+func Test_MessagesService_SearchMessages_errorResponse(t *testing.T) {
+	from := time.Date(2018, time.May, 1, 0, 0, 0, 0, time.Local)
+	to := time.Date(2018, time.May, 3, 0, 0, 0, 0, time.Local)
+	query := &SearchMessagesOptions{TopicIDs: []int{}, HasAttachments: true, AccountIDs: []int{}, From: &from, To: &to}
+	_, _, err := client.Messages.SearchMessages(context.Background(), "qwerty", "hello", query)
+
+	if err == nil {
+		t.Errorf("Expected error to be returned")
 	}
 }
